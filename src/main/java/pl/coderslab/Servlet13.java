794861bea5129fecebc06dd821e188a41f4cd026
Krzysztof Tomala <krzysiektom@op.pl>
2018-12-24 12:09:50 +0100

    Zadanie 3

---
+++ src/main/java/pl/coderslab/Servlet13.java
@@ -0,0 +1,26 @@
+package pl.coderslab;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@WebServlet("/Servlet13")
+public class Servlet13 extends HttpServlet {
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html; charset=UTF-8");
+            Path path1 = Paths.get("/home/krzysztof/workspace/Homework_02/src/oop.txt");
+            try {
+                for (String line : Files.readAllLines(path1)) {
+                    response.getWriter().append(line).append("<br>");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+    }
+}
